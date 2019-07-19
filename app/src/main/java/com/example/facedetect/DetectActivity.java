ef0d8ba6ff702ac773440e37ef4afd6c8340edf0
Arjun Rao <arjunarao619@gmail.com>
2019-07-19 21:45:39 +0800

    Added Image POST using multipart-entity

--- app/src/main/java/com/example/facedetect/DetectActivity.java
+++ app/src/main/java/com/example/facedetect/DetectActivity.java
@@ -39,10 +39,17 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
 import static com.google.android.gms.vision.face.FaceDetector.ACCURATE_MODE;
@@ -54,6 +61,7 @@ public class DetectActivity extends AppCompatActivity {
     Camera camera;
     float xpos,ypos,width,height;
     EditText photoname;
+    Bitmap myBitmap;
 
 
 
@@ -95,10 +103,11 @@ public class DetectActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
         if(requestCode == Camera.REQUEST_TAKE_PHOTO){
-            Bitmap myBitmap = camera.getCameraBitmap();
+             myBitmap = camera.getCameraBitmap();
             if(myBitmap != null) {
 
                 final ImageView imageView2 = (ImageView) findViewById(R.id.test);
+
 
                 //imageView2.setImageBitmap(myBitmap);
                 Paint myRectPaint = new Paint();
@@ -180,10 +189,10 @@ public class DetectActivity extends AppCompatActivity {
                 @Override
                 public void onClick(DialogInterface dialogInterface, int i) {
                     EditText personname = customLayout.findViewById(R.id.personname);
-                    photoname = customLayout.findViewById(R.id.photoname);
-                    EditText photourl = customLayout.findViewById(R.id.photourl);
+                   // photoname = customLayout.findViewById(R.id.photoname);
+                   // EditText photourl = customLayout.findViewById(R.id.photourl);
                     EditText personid = customLayout.findViewById(R.id.personid);
-                    //TODO SEND JSON POST REQUEST
+
 
                     JSONObject json = new JSONObject();
                     try{
@@ -191,8 +200,9 @@ public class DetectActivity extends AppCompatActivity {
 
                         json.put("person_name",personname.getText().toString());
                         json.put("person_id",personid.getText().toString());
-                        json.put("photo_name",photoname.getText().toString());
-                        json.put("photo_url",photourl.getText().toString());
+                        json.put("photo_name",personid.getText().toString());
+                        String photoURL = "http://192.168.7.115/api/v1/showface/image/" + personid.getText().toString();
+                        json.put("photo_url",photoURL);
                     }catch(JSONException e){
                         e.printStackTrace();
                     }
@@ -230,14 +240,6 @@ public class DetectActivity extends AppCompatActivity {
                     JSONArray faceContour = new JSONArray();
                     faceContour.put(face_contour);
                     faceContour.put(face_contour2);
-
-
-                    /*JSONObject facecontour = new JSONObject();
-                    try{
-                    facecontour.put("face_contour",faceContour);}
-                    catch (JSONException e){
-                        e.printStackTrace();
-                    }*/
 
                     JSONObject landmark_vector = new JSONObject();
                     try{
@@ -318,6 +320,126 @@ public class DetectActivity extends AppCompatActivity {
 
         thread.start();
     }
+
+
+    public String multipartRequest(String urlTo, Map<String, String> parmas, String filepath, String filefield, String fileMimeType) throws Exception {
+        HttpURLConnection connection = null;
+        DataOutputStream outputStream = null;
+        InputStream inputStream = null;
+
+        String twoHyphens = "--";
+        String boundary = "*****" + Long.toString(System.currentTimeMillis()) + "*****";
+        String lineEnd = "\r\n";
+
+        String result = "";
+
+        int bytesRead, bytesAvailable, bufferSize;
+        byte[] buffer;
+        int maxBufferSize = 1 * 1024 * 1024;
+
+        String[] q = filepath.split("/");
+        int idx = q.length - 1;
+
+        try {
+            File file = new File(filepath);
+            FileInputStream fileInputStream = new FileInputStream(file);
+
+            URL url = new URL(urlTo);
+            connection = (HttpURLConnection) url.openConnection();
+
+            connection.setDoInput(true);
+            connection.setDoOutput(true);
+            connection.setUseCaches(false);
+
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Connection", "Keep-Alive");
+            connection.setRequestProperty("User-Agent", "Android Multipart HTTP Client 1.0");
+            connection.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundary);
+
+            outputStream = new DataOutputStream(connection.getOutputStream());
+            outputStream.writeBytes(twoHyphens + boundary + lineEnd);
+            outputStream.writeBytes("Content-Disposition: form-data; name=\"" + filefield + "\"; filename=\"" + q[idx] + "\"" + lineEnd);
+            outputStream.writeBytes("Content-Type: " + fileMimeType + lineEnd);
+            outputStream.writeBytes("Content-Transfer-Encoding: binary" + lineEnd);
+
+            outputStream.writeBytes(lineEnd);
+
+            bytesAvailable = fileInputStream.available();
+            bufferSize = Math.min(bytesAvailable, maxBufferSize);
+            buffer = new byte[bufferSize];
+
+            bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+            while (bytesRead > 0) {
+                outputStream.write(buffer, 0, bufferSize);
+                bytesAvailable = fileInputStream.available();
+                bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+            }
+
+            outputStream.writeBytes(lineEnd);
+
+            // Upload POST Data
+            Iterator<String> keys = parmas.keySet().iterator();
+            while (keys.hasNext()) {
+                String key = keys.next();
+                String value = parmas.get(key);
+
+                outputStream.writeBytes(twoHyphens + boundary + lineEnd);
+                outputStream.writeBytes("Content-Disposition: form-data; name=\"" + key + "\"" + lineEnd);
+                outputStream.writeBytes("Content-Type: text/plain" + lineEnd);
+                outputStream.writeBytes(lineEnd);
+                outputStream.writeBytes(value);
+                outputStream.writeBytes(lineEnd);
+            }
+
+            outputStream.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+
+
+            if (200 != connection.getResponseCode()) {
+                throw new Exception("Failed to upload code:" + connection.getResponseCode() + " " + connection.getResponseMessage());
+            }
+
+            inputStream = connection.getInputStream();
+
+            result = this.convertStreamToString(inputStream);
+
+            fileInputStream.close();
+            inputStream.close();
+            outputStream.flush();
+            outputStream.close();
+
+            return result;
+        } catch (Exception e) {
+           // logger.error(e);
+            throw new Exception(e);
+        }
+
+    }
+
+    private String convertStreamToString(InputStream is) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+
+        String line = null;
+        try {
+            while ((line = reader.readLine()) != null) {
+                sb.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return sb.toString();
+    }
+
+
+
+
 
 
 
